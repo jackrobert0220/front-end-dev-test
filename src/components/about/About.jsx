d5e60d8900136898c4d0a3653fff36a1fce41333
jackrobert0220 <jackrobert0220@gmail.com>
2022-05-18 14:29:44 -0700

    started quadrant comp

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,9 +5,9 @@ export default function About() {
     <div className="about">
         <div className="left">
             <div className="wrapper">
-                <h1>Lorem Ipsum</h1>
+                <h1>LOREM IPSUM</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
-                <a href="https://www.google.com/">
+                <a href="https://www.jackglazer.com/">
                     <h2>&gt; Lorem Ipsum</h2>
                 </a>
             </div>
--- src/components/quadrant/Quadrant.jsx
+++ src/components/quadrant/Quadrant.jsx
@@ -3,7 +3,20 @@ import "./quadrant.scss";
 export default function Quadrant() {
   return (
     <div className="quadrant">
-    Quadrant
+        <div className="left">
+            <div className="wrapper">
+                <h1>LOREM IPSUM</h1>
+                <h1>DOLOR SIT AMET</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
+                <a href="https://www.jackglazer.com/">
+                    <h2>&gt; Lorem Ipsum</h2>
+                </a>
+            </div>
+        </div>
+        <div className="right">
+          
+        </div>
+
     </div>
   )
 }
