b564e0c1c87dcd048419f53d03903f7cac759d75
jackrobert0220 <jackrobert0220@gmail.com>
2022-05-17 19:05:18 -0700

    welcome component styling finished

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -3,8 +3,18 @@ import "./welcome.scss";
 export default function Welcome() {
     return (
         <div className="welcome">
+                {/* Upper Left Logo */}
                 <div className="logo">
                     <img src="../../../assets/logo-white.svg" alt=""/>
+                </div>
+
+                <div className="title">
+                    <p className="lineOne">WELCOME TO</p>      
+                    <p className="lineTwo">Rooster Grin</p>
+                </div>
+
+                <div className="buttonWrap">
+                    <button className="buttonOne">Button Button</button>
                 </div>
         </div>
     )
