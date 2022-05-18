fea44167d0a94f3ebbd5996daa349f2884e15953
jackrobert0220 <jackrobert0220@gmail.com>
2022-05-18 13:16:08 -0700

    about component complete

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,13 +3,15 @@ import "./about.scss";
 export default function About() {
   return (
     <div className="about">
-        <dvi className="left">
+        <div className="left">
+            <div className="wrapper">
                 <h1>Lorem Ipsum</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
                 <a href="https://www.google.com/">
                     <h2>&gt; Lorem Ipsum</h2>
                 </a>
-        </dvi>
+            </div>
+        </div>
 
         <div className="right">
             <img src="./assets/img-1.jpg" alt="" />
