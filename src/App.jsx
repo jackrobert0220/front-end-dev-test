7ffeeb625749629a96e18d929e2cb69b77340cc2
jackrobert0220 <jackrobert0220@gmail.com>
2022-05-19 18:30:21 -0700

    started footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import About from "./components/about/About";
 import Learn from "./components/learn/Learn";
 import Quadrant from "./components/quadrant/Quadrant";
 import Scream from "./components/scream/Scream";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="app">
-      <Welcome />
-      <About />
-      <Learn />
-      <Quadrant />
-      <Scream />
+      <div className="content">
+        <Welcome />
+        <About />
+        <Learn />
+        <Quadrant />
+        <Scream />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./footer.scss";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div className="wrapper">
+                <h3>Follow Us:</h3>
+                    <div className="imageWrap">
+                        <a href="https://www.jackglazer.com"><img src="./assets/instagram.svg" alt=""/></a>     
+                        <a href="https://www.jackglazer.com"><img src="./assets/facebook.svg" alt=""/></a>     
+                    </div>
+            </div>
+        </div>
+    )
+}
