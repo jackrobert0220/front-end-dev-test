584b283936af694f590e5fa810966dc781a03f52
jackrobert0220 <jackrobert0220@gmail.com>
2022-05-23 15:12:13 -0700

    redid welcome page

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,25 +1,33 @@
 import "./welcome.scss";
 
 export default function Welcome() {
-    return (
-        <div className="welcome">
-                {/* Upper Left Logo */}
-                <div className="logo">
-                    <img src="../../../assets/logo-white.svg" alt=""/>
-                </div>
-
-                <div className="title">
-                    <p className="lineOne">WELCOME TO</p>      
-                    <p className="lineTwo">Rooster Grin</p>
-                </div>
-
-                <div className="buttonWrap">
-                    <button className="buttonOne" onClick={() => {
-      window.open(
-        "https://www.jackglazer.com",
-        "_blank");
-    }}>Button Button</button>
-                </div>
+  return (
+    <div className="welcome">
+        <div className="welcome__hero-image"></div>
+        <div className="welcome__container">
+        {/* Upper Left Logo */}
+        <div className="welcome__logo">
+            <img src="../../../assets/logo-white.svg" alt="" />
         </div>
-    )
-};
+
+        <div className="welcome__title">
+          {/* make sure to use semantic tags */}
+            <h2 className="welcome__title--line-one">WELCOME TO</h2>
+            <h2 className="welcome__title--line-two">Rooster Grin</h2>
+        </div>
+
+        <div className="welcome__button-wrapper">
+          {/* I prefer an href here. If you were linking within your site look into react-router */}
+            <a
+            className="welcome__button-content"
+            href="https://www.jackglazer.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Button Button
+            </a>
+        </div>
+        </div>
+    </div>
+    );
+}
