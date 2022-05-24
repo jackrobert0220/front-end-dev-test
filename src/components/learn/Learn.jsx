55c32a2bb97072b3c111348fe7a5bba76918ae2b
jackrobert0220 <jackrobert0220@gmail.com>
2022-05-24 16:43:57 -0700

    learn comp styled

--- src/components/learn/Learn.jsx
+++ src/components/learn/Learn.jsx
@@ -3,7 +3,7 @@ import "./learn.scss";
 export default function Learn() {
   return (
     <div className="learn">
-        <div className="wrapper">
+        <div className="learn__wrapper">
             <h1>LOREM IPSUM</h1>
             <button onClick={() => {
       window.open(
